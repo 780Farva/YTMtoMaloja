2b1a5e316590b1a0a28bb7cd5c134eb012367f2a
Jules LaPrairie <jules.laprairie@derbi.io>
2025-03-07 07:08:15 -0700

    retrieving additional data for distinct artist-track combinations from musicbrainz

--- src/root_command.sh
+++ src/root_command.sh
@@ -4,9 +4,10 @@ log info "~*~*~*~ LastTakeoutScraper ~*~*~*~"
 # tmp_folder=/tmp/lts/
 tmp_folder=./tmp/lts
 music_entries_only_file=${tmp_folder}/music_only.json
-artist_names_file=${tmp_folder}/artists.txt
+unique_artist_tracks=${tmp_folder}/artist_tracks.json
+tagged_uniques=${tmp_folder}/tagged_artist_tracks.json
 
-log info "I'll read from ${args[source]}, make some temporary files in ${tmp_folder} and write my results to ${args[out]}."
+log info "I'll read from ${args[source]}, make some temporary files in ${tmp_folder} and write my results to ${args[out]}"
 
 mkdir -p $tmp_folder
 
@@ -22,12 +23,49 @@ jq '[.[] | select(.header == "YouTube Music") |
 
 log debug "A music-only version of the input file has been written to ${music_entries_only_file}"
 
-log info "Extracting artist names"
-# write just the artists to a file
-jq -r 'group_by(.artist) | map({artist: .[0].artist}) | .[].artist' ${music_entries_only_file} > ${artist_names_file}
+log debug "Collecting unique artist-track pairs..."
+jq '[unique_by([ .artist, .track ]) | .[] | del(.uts,.utc_time)]' ${music_entries_only_file} > ${unique_artist_tracks}
 
-echo "uts,utc_time,artist,track" > ${args[out]}
-jq -r '.[] | [.uts, .utc_time, .artist, .track] | @csv' \
-	${music_entries_only_file} >> ${args[out]}
+
+
+# a function that gets additional track information from musicbrainz
+# usage get-mb-data <track_name> <artist_name>
+function get-mb-data () {
+mb_result=$(curl -s --get \
+--data-urlencode 'fmt=json' \
+--data-urlencode 'limit=1' \
+--data-urlencode "query=\"$1\" AND artist:\"$2\"" \
+'https://musicbrainz.org/ws/2/recording/') 
+
+echo $mb_result | jq -M 'select(.count != 0) |
+. += {
+		track: ( .recordings[0].title ),
+	  track_mbid: ( .recordings[0].id ),
+		artist: ( .recordings[0]."artist-credit"[0].name),
+		artist_mbid: ( .recordings[0]."artist-credit"[0].artist.id),
+		album: ( .recordings[0].releases[0].title ),
+		album_mbid: ( .recordings[0].releases[0].id )} |
+del(.created, .count, .offset, .recordings)'
+}
+
+# use those artist names to get info from musicbrainz
+log info "Retrieving album data..."
+echo "[" > $tagged_uniques
+jq -c '.[]' < $unique_artist_tracks | while read -r line; do
+  artist_name=$(echo "$line" | jq -r '.artist')
+  track_name=$(echo "$line" | jq -r '.track')
+		log debug  $artist_name $track_name
+  recording=$(get-mb-data "$track_name" "$artist_name")
+  if [[ -n $recording ]]; then
+    # Capture the result into a new file
+		log debug "."
+    echo "${recording}," >> $tagged_uniques
+  fi
+done
+
+
+# echo "uts,utc_time,artist,track" > ${args[out]}
+# jq -r '.[] | [.uts, .utc_time, .artist, .track] | @csv' \
+# 	${music_entries_only_file} >> ${args[out]}
 
 log info "~*~*~*~ fin ~*~*~*~"
